072d50b94e4d97a7251d55d5b2ce51910d9b7abe
andy-keene <keene.andy.t@gmail.com>
2018-02-26 21:39:42 -0800

    init commit: add basic pipeline for react -> extension

---
+++ src/components/app.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react'
+
+class Popup extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                Hello World!
+            </div>
+        );
+    }
+}
+
+export default Popup;
---
+++ src/popup.js
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Popup from './components/app';
+
+ReactDOM.render(<Popup/>, document.getElementById('root'));
