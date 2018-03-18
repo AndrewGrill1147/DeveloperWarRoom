1c7b717deab947ec52b97d9127acb7b47d4850b4
Minh Phan <minphan@pdx.edu>
2018-03-18 16:18:09 -0700

    Extension tab can only be opened once

--- src/background.js
+++ src/background.js
@@ -1,6 +1,24 @@
 /* global chrome */
+function IsExtensionTabOpened(url, callback) {
+  var result;
+  chrome.tabs.query({ 'active': true, 'currentWindow': true }, function (tabs) {
+    tabs.forEach(function (tab) {
+      if (tab.url.includes(url)) {
+        result = true;
+      }
+    });
+    return callback(result);
+  }
+  )
+}
 
 /* Opens app when browser icon (action) is clicked */
 chrome.browserAction.onClicked.addListener(() => {
-  chrome.tabs.create({ url: chrome.extension.getURL('app.html'), selected: true });
+  var url = "app.html";
+  IsExtensionTabOpened(url, function (result) {
+    if (!result) {
+      chrome.tabs.create({ url: chrome.extension.getURL(url), selected: true });
+    }
+  });
 });
+
