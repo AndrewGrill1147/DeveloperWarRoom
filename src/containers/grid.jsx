21c467c6ec3c82b6af611e1c8f66feccfa3418c4
Brandon Bauley <bbauley@pdx.edu>
2018-04-14 16:59:04 -0700

    Made a button and on click it will change from edit to normal mode. Need to make it more dynamic by keeping coordinates the same when changing from normal to edit mode.

--- src/containers/grid.jsx
+++ src/containers/grid.jsx
@@ -2,6 +2,8 @@ import ReactGridLayout from 'react-grid-layout';
 import React, { Component } from 'react';
 import TodoList from './todoList';
 import RaisedButton from 'material-ui/RaisedButton';
+import ExampleLayout from './testHook';
+import RGL, { WidthProvider } from "react-grid-layout";
 
 // just for testing react-grid
 const divStyle = {
@@ -9,22 +11,13 @@ const divStyle = {
   fontWeight: 'bold',
   backgroundColor: 'coral',
 };
-{/*const layout = [
-  {
-    i: 'a', x: 0, y: 0, w: 3, h: 5, minH: 5, minW: 3,
-  },
-  {
-    i: 'b', x: 1, y: 0, w: 3, h: 5, minH: 5, minW: 3,
-  },
-  {
-    i: 'c', x: 4, y: 0, w: 3, h: 5, minH: 5, minW: 3,
-  },
-];*/}
-class Grid extends Component {
+
+class Grid extends React.PureComponent {
   constructor(props) {
     super(props);
     console.log(this.editMode);
     this.editButtonClicked = this.editButtonClicked.bind(this),
+    this.onLayoutChange = this.onLayoutChange.bind(this),
     this.state = { 
 
       editMode: false,
@@ -41,13 +34,12 @@ class Grid extends Component {
       ]
     };
   }
-/*
-  onToggle(toggledTodo) {
-    const updatedTodos = this.state.todos.map(todo => (todo !== toggledTodo ?
-      todo :
-      { ...todo, ...{ completed: !todo.completed } }));
-    this.setState({ todos: updatedTodos });
-  }*/
+
+  onLayoutChange(layout) {
+    /*this.props.onLayoutChange(layout);*/
+    this.setState({layout: layout});
+  }
+
   editButtonClicked() {
     console.log("In editButtonClicked");
     {/*console.log(this.state.editMode);*/}
@@ -67,18 +59,7 @@ class Grid extends Component {
       },
     ] 
     this.setState( {layout: updateLayout});
-    /*this.setState({ layout: 
-    [
-      {
-        i: 'a', x: 0, y: 0, w: 3, h: 5, minH: 5, minW: 3, static: this.state.editMode,
-      },
-      {
-        i: 'b', x: 1, y: 0, w: 3, h: 5, minH: 5, minW: 3, static: this.state.editMode,
-      },
-      {
-        i: 'c', x: 4, y: 0, w: 3, h: 5, minH: 5, minW: 3, static: this.state.editMode,
-      },
-    ] });*/
+
   }
   
   componentDidMount() {
@@ -86,14 +67,14 @@ class Grid extends Component {
     console.log("In componentDidMount");
     console.log(this.state.editMode);
 
-    //const savedState = localStorage.getItem(this.state.storageKey);
-    //if (savedState !== null) {
+    const savedState = localStorage.getItem(this.state.storageKey);
+    if (savedState !== null) {
       /* eslint-disable-next-line */
-     // this.setState(JSON.parse(savedState));
-      // this.state.storageKey = JSON.parse(savedState);
-    //}
+      this.setState(JSON.parse(savedState));
+      this.state.storageKey = JSON.parse(savedState);
+    }
     /* global window */
-    //window.addEventListener('beforeunload', this.componentWillUnmount);
+    window.addEventListener('beforeunload', this.componentWillUnmount);
   }
 
   componentWillUnmount() {
@@ -108,8 +89,8 @@ class Grid extends Component {
     return (
       <div>
       <RaisedButton onClick={this.editButtonClicked} label="Edit" />
-      <ReactGridLayout className="layout" draggableCancel="input,textarea" layout={this.state.layout} cols={12} rowHeight={30} width={1200}>
-        <div style={divStyle} key="a">a</div>
+      <ReactGridLayout className="layout" onLayoutChange={this.onLayoutChange} draggableCancel="input,textarea" layout={this.state.layout} cols={12} rowHeight={30} width={1200}>
+        <div key="a"><TodoList/></div>
         <div style={divStyle} key="b">b</div>
         <div style={divStyle} key="c">c</div>
       </ReactGridLayout>
@@ -118,4 +99,9 @@ class Grid extends Component {
   }
 }
 
+
 export default Grid;
+/*module.exports = Grid;
+if (require.main === module) {
+  require("./testHook.jsx")(module.exports);
+}*/
