04740bf3e2070d0c3bd0b5d69cd630ce80787d96
Brandon Bauley <bbauley@pdx.edu>
2018-04-15 17:29:19 -0700

    Can add and remove widgets. Trying to sync the features to only work when in edit mode.

--- src/containers/grid.jsx
+++ src/containers/grid.jsx
@@ -4,6 +4,12 @@ import TodoList from './todoList';
 import AppBar from 'material-ui/AppBar';
 import FlatButton from 'material-ui/FlatButton';
 import _ from 'lodash';
+/** **************************** */
+// added for alternate add/remove
+import { WidthProvider, Responsive } from 'react-grid-layout';
+
+const ResponsiveReactGridLayout = WidthProvider(Responsive);
+/** **************************** */
 
 const appBarStyle = {
   backgroundColor: 'gray',
@@ -15,117 +21,180 @@ const divStyle = {
   backgroundColor: 'coral',
 };
 
-const removeStyle = {
+const defaultProps = {
+  className: 'layout',
+  cols: {
+    lg: 12, md: 10, sm: 6, xs: 4, xxs: 2,
+  },
+  rowHeight: 100,
+};
+/** **************************** */
+// removed for alternate add/remove
+/* const removeStyle = {
   position: 'absolute',
   right: '2px',
   top: 0,
   cursor: 'pointer',
-};
+}; */
+/** **************************** */
+
 
 class Grid extends Component {
   constructor(props) {
     super(props);
-    // console.log(this.editMode);
     this.editButtonClicked = this.editButtonClicked.bind(this),
     this.onLayoutChange = this.onLayoutChange.bind(this),
+    this.onAddItem = this.onAddItem.bind(this);
+    this.onBreakpointChange = this.onBreakpointChange.bind(this);
     this.state = {
       editMode: false,
-      // initializing states for widgets
-      layout: [
-        {
-          i: 'a', x: 0, y: 0, w: 3, h: 5, minH: 5, minW: 3, static: true,
-        },
-        {
-          i: 'b', x: 1, y: 0, w: 3, h: 5, minH: 5, minW: 3, static: true,
-        },
-        {
-          i: 'c', x: 4, y: 0, w: 3, h: 5, minH: 5, minW: 3, static: true,
-        },
-      ],
+      counter: 0,
+      layout: [0, 1, 2].map((i, key, list) => ({
+        i: i.toString(),
+        x: i * 2,
+        y: 0,
+        w: 2,
+        h: 2,
+        add: i === (list.length - 1).toString(),
+        static: true,
+      })),
     };
   }
 
-  componentDidMount() {
-    /* global localStorage */
+  /* componentDidMount() {
+    // global localStorage
     const savedState = localStorage.getItem(this.state.storageKey);
     if (savedState !== null) {
-      /* eslint-disable-next-line */
+      // eslint-disable-next-line
       this.setState(JSON.parse(savedState));
       this.state.storageKey = JSON.parse(savedState);
     }
-    /* global window */
+    // global window
     window.addEventListener('beforeunload', this.componentWillUnmount);
-  }
+  } */
 
+  /*
   componentWillUnmount() {
-    /* global localStorage */
+    // global localStorage
     // console.log('In componentWillUnMount');
     localStorage.setItem(this.state.storageKey, JSON.stringify(this.state));
-    /* global window */
+    // global window
     window.removeEventListener('beforeunload', this.componentDidUpdate);
-  }
+} */
 
   onLayoutChange(layout) {
+    // console.log("In onLayoutChange");
     this.setState({ layout });
   }
+
   editButtonClicked() {
-    // console.log('In editButtonClicked');
+    console.log('In editButtonClicked');
     const flipped = !this.state.editMode;
     this.setState({ editMode: flipped });
     const updatedLayout = [...this.state.layout].map(widgetLayout => ({ ...widgetLayout, ...{ static: flipped } }));
     this.setState({ layout: updatedLayout });
   }
 
-  onRemoveItem(key) {
-    // console.log('removing', key);
-    this.setState({ layout: _.reject(this.state.layout, { i: key }) });
+  onRemoveItem(i) {
+    console.log('removing', i);
+    this.setState({ layout: _.reject(this.state.layout, { i }) });
+  }
+
+  createElement(el) {
+    console.log('In createElement');
+    console.log(el);
+    // Inline style for X in top right corner of widgets
+    const removeStyle = {
+      position: 'absolute',
+      right: '2px',
+      top: 0,
+      cursor: 'pointer',
+    };
+    const i = el.add ? '+' : el.i;
+    return (
+      <div style={divStyle} key={i} data-grid={el}>
+        {el.add ? (
+          <span
+            className="add text"
+            onClick={this.onAddItem}
+            title="You can add an item by clicking here, too."
+          >
+            Add +
+          </span>
+        ) : (
+          <span className="text">{i}</span>
+        )}
+        <span
+          className="remove"
+          style={removeStyle}
+          onClick={this.onRemoveItem.bind(this, i)}
+        >
+          x
+        </span>
+      </div>
+    );
+  }
+
+  onAddItem() {
+    /* eslint no-console: 0 */
+    console.log('adding', 'n', this.state.counter);
+    this.setState({
+      // Add a new item. It must have a unique key!
+      layout: this.state.layout.concat({
+        i: `n${this.state.counter}`,
+        x: (this.state.layout.length * 2) % (this.state.cols || 12),
+        y: Infinity, // puts it at the bottom
+        w: 2,
+        h: 2,
+        static: false,
+      }),
+      // Increment the counter to ensure key is always unique.
+      counter: this.state.counter + 1,
+    });
+  }
+
+  onBreakpointChange(breakpoint, cols) {
+    // console.log("In onBreakpointChange");
+    this.setState({
+      breakpoint,
+      cols,
+    });
   }
 
   render() {
-    // console.log('render');
-    // console.log(this);
+    //Probably will need to change this(maybe not)
     if (this.state.layout[0].static == true) {
       return (
         <div>
-          <AppBar style={appBarStyle} iconElementRight={<FlatButton label="Edit" onClick={this.editButtonClicked} />} />
-          <ReactGridLayout className="layout" onLayoutChange={this.onLayoutChange} draggableCancel="input,textarea" layout={this.state.layout} cols={12} rowHeight={30} width={1200}>
-            <div key="a"><TodoList /></div>
-            <div style={divStyle} key="b">b</div>
-            <div style={divStyle} key="c">c</div>
-          </ReactGridLayout>
+          <AppBar
+            style={appBarStyle}
+            iconElementRight={<FlatButton label="Edit" onClick={this.editButtonClicked} />}
+          />
+          <ResponsiveReactGridLayout
+            onLayoutChange={this.onLayoutChange}
+            onBreakpointChange={this.onBreakpointChange}
+            {...this.props}
+          >
+            {_.map(this.state.layout, el => this.createElement(el))}
+          </ResponsiveReactGridLayout>
         </div>
       );
     }
 
     return (
       <div>
-        <AppBar style={appBarStyle} iconElementRight={<FlatButton label="Confirm" onClick={this.editButtonClicked} />} />
-        <ReactGridLayout className="layout" onLayoutChange={this.onLayoutChange} draggableCancel="input,textarea" layout={this.state.layout} cols={12} rowHeight={30} width={1200}>
-          <div key="a"><TodoList />
-            <span
-              className="remove"
-              style={removeStyle}
-              onClick={this.onRemoveItem.bind(this, 'a')}
-            >X
-            </span>
-          </div>
-          <div style={divStyle} key="b">b
-            <span
-              className="remove"
-              style={removeStyle}
-              onClick={this.onRemoveItem.bind(this, 'b')}
-            >X
-            </span>
-          </div>
-          <div style={divStyle} key="c">c
-            <span
-              className="remove"
-              style={removeStyle}
-              onClick={this.onRemoveItem.bind(this, 'c')}
-            >X
-            </span>
-          </div>
-        </ReactGridLayout>
+        <AppBar
+          style={appBarStyle}
+          iconElementRight={<FlatButton label="Confirm" onClick={this.editButtonClicked} />}
+        />
+        <button onClick={this.onAddItem}>Add Item</button>
+        <ResponsiveReactGridLayout
+          onLayoutChange={this.onLayoutChange}
+          onBreakpointChange={this.onBreakpointChange}
+          {...this.props}
+        >
+          {_.map(this.state.layout, el => this.createElement(el))}
+        </ResponsiveReactGridLayout>
       </div>
     );
   }
