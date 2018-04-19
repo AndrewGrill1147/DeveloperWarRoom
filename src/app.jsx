c98a6669ce78af470836a989f650d1b3defe6f40
vmannn <vicochia@pdx.edu>
2018-04-18 20:42:20 -0700

    Update app.jsx

--- src/app.jsx
+++ src/app.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import Home from './components/home';
 import './index.css';
 
+const App = () => (
+  <MuiThemeProvider>
+    <Home />
+  </MuiThemeProvider>
+);
 /* global document */
-ReactDOM.render(<Home />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
