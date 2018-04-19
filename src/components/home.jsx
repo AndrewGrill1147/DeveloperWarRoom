6c0daf3572845b03f0c90acf678da60d9f638a70
vmannn <vicochia@pdx.edu>
2018-04-18 20:43:15 -0700

    Update home.jsx

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import Grid from './grid';
-/* eslint react/prefer-stateless-function: 0 */
+import Bookmarkers from './bookmarker';
 
+/*
+ * might be wise to rename this file since it's imported by another
+ * popup.js file..
+ */
 class Home extends Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <div>
-        This is the applications home page.
+        
+        <Bookmarkers />
+        
         <Grid />
       </div>
     );
