763e5e0fc71667d22499d35d6ed757828d2fd43e
Minh Phan <minh.phan@fei.com>
2018-05-22 16:10:54 -0700

    fix github widget div, paper style

--- src/components/Github/index.jsx
+++ src/components/Github/index.jsx
@@ -40,6 +40,16 @@ const styles = {
     width: '5%',
     marginTop: '27px',
   },
+
+  divStyle: {
+    width: '100%',
+    height: '100%',
+  },
+  expand: {
+    maxHeight: '100%',
+    overflow: 'auto',
+  },
+
   superSelectField: {
     marginTop: '45px',
     fontSize: '16px',
@@ -382,8 +392,8 @@ class GithubWidget extends Component {
     }
 
     return (
-      <div>
-        <Paper>
+      <div style={styles.divStyle} >
+        <Paper style={styles.expand} >
           <Tabs>
             <Tab icon={<PullRequestIcon />}>
               {errorMessage}
