7bf01f8a24ff076a8f8bf450a7337c9ed585c489
Brandon Bauley <bbauley@pdx.edu>
2018-05-28 17:38:48 -0700

    Update bookmarker.test.js

--- test/bookmarker.test.js
+++ test/bookmarker.test.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Bookmarker from '../src/components/Bookmarker';
 import { Dialog, IconButton, TextField } from 'material-ui';
 
-
-
 /* Testing the Bookmarker Component */
 describe('<Bookmarker />', () => {
   it('add bookmark button changes state', () => {
@@ -11,12 +9,6 @@ describe('<Bookmarker />', () => {
     wrapper.find(IconButton).simulate('click');
     expect(wrapper.state().dialogOpen).toEqual(true);
   });
-  /*it('add bookmark Edit Delete button changes state', () => {
-   // error FlatButton not defined
-    const wrapper = shallow(<Bookmarker />);
-    wrapper.find(FlatButton).simulate('click');
-    expect(wrapper.state().dialogOpen).toEqual(true);
-  }); */
   it('add and delete widget', () => {
     const wrapper = shallow(<Bookmarker />);
     const value = {id: 29, nameInput: 'test', urlInput: 'www.google.com', dialogOpen: true};
@@ -56,26 +48,4 @@ describe('<Bookmarker />', () => {
       wrapper.instance().handleUrlChange(urlValue, value7);
       expect(wrapper.state().bookmarks[0].urlInput).toEqual('www.allclassical.org');
     }); 
-
 });
-
-
-
-
-/*
- Testing the Bookmarker Component 
-describe('<Bookmarker />', () => {
-  it('add bookmark button changes state', () => {
-    const wrapper = shallow(<Bookmarker />);
-    wrapper.find(IconButton).simulate('click');
-    expect(wrapper.state().dialogOpen).toEqual(true);
-  });
-  it('add quick link', () => {
-    const wrapper = shallow(<Bookmarker />);
-    const value = {id: 10, nameInput: 'test', urlInput: 'www.google.com', dialogOpen: true};
-    const value2 = {id: 4, nameInput: 'test2', urlInput: 'www.youtube.com', dialogOpen: true};
-    wrapper.instance().saveDialogEvent("key", value);
-    wrapper.instance().saveDialogEvent("key2", value2);
-    expect(wrapper.state().bookmarks.length).toEqual(2);
-  });
-});*/
